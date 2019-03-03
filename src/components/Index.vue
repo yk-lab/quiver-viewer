38b1a2193944c35d21af8dc8727c1720df7c1b3c
yk-lab <yetanother.yk@gmail.com>
2019-03-04 03:44:03 +0900

    Topページ下地

---
+++ src/components/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <router-link tag="a" to="/app">App</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+}
+</script>
+
+<style scoped>
+</style>
